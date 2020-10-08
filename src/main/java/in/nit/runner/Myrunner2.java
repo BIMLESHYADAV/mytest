519bd6a60209f0820670ef3d800f5a303d708f1d
91854 <91854@192.168.0.5>
2020-10-08 19:01:59 +0530

    mytest commit3

---
+++ src/main/java/in/nit/runner/Myrunner2.java
@@ -0,0 +1,14 @@
+package in.nit.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Myrunner2 implements CommandLineRunner
+{
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to springboot command line runner 2 ");
+		System.out.println("welcome to java commandline runner 2 ");
+		
+	}
+
+}
